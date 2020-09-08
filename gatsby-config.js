06665f1c71758e17d8082b7ed499268fcf6c31e1
youknowcast <ykc@daycrift.net>
2020-09-08 23:38:52 +0900

    add github action yml

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
+    `gatsby-plugin-twitter`,
     `gatsby-plugin-netlify`,
     shouldAnalyseBundle && {
       resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
