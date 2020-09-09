834e061668e11298c67175e7c39b85600bf169bb
youknowcast <ykc@daycrift.net>
2020-09-10 02:24:58 +0900

    add 9/10 blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,6 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 1035,
-              sizeByPixelDensity: true,
             },
           },
           {
