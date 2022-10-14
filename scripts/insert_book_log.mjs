c7096b371467849f5e899086b12272d4602d5e11
Ambe Hideki <ykc@daycrift.net>
2022-10-14 14:32:53 +0900

    Add insert_book_log.mjs

---
+++ scripts/insert_book_log.mjs
@@ -0,0 +1,24 @@
+#!/usr/bin/env zx
+
+//
+// generate today blog template
+// requirements:
+//   [zx](https://github.com/google/zx)
+//   installation: `yarn global add zx`
+// usage:
+//   % cd /path/to/daycrift-root
+//   % ./scripts/insert_book_log {book_name} --date 2022-10-14
+// --date is optional. default date is current day
+//
+
+const BOOK_LOG_MDX_PATH = "content/posts/2022/10/20221014/index.mdx"
+
+const bookName = argv._[1]
+if (bookName === undefined) {
+  console.error("book_name is not specified")
+  await $`exit`
+}
+const date = argv.date || (new Date()).toISOString().substr(0, 10)
+
+const bookLog = `\n|${bookName}|${date}||`
+fs.outputFileSync(BOOK_LOG_MDX_PATH, bookLog, {flag: "a+"})
