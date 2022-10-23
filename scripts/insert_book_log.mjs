ccdb582c44580f6a29c57b50b33dd82ba1f0ce12
Ambe Hideki <ykc@daycrift.net>
2022-10-23 22:04:39 +0900

    update script

--- scripts/insert_book_log.mjs
+++ scripts/insert_book_log.mjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env zx
 
 //
-// generate today blog template
+// register read book log to current book log
 // requirements:
 //   [zx](https://github.com/google/zx)
 //   installation: `yarn global add zx`
@@ -22,3 +22,10 @@ const date = argv.date || (new Date()).toISOString().substr(0, 10)
 
 const bookLog = `\n|${bookName}|${date}||`
 fs.outputFileSync(BOOK_LOG_MDX_PATH, bookLog, {flag: "a+"})
+
+const commitLog = "update book log"
+
+await $`git add ${BOOK_LOG_MDX_PATH}`
+await $`git commit -m "${commitLog}"`
+
+console.log("updated book log and committed.")
