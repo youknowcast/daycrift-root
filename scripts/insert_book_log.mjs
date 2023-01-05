0021ad0ea49ac36e3857bc4ae80e83b6379d6a38
Ambe Hideki <ykc@daycrift.net>
2023-01-05 23:27:10 +0900

    chore: fix `insert_book_log` zx script. targeting 2023, fix git log message

--- scripts/insert_book_log.mjs
+++ scripts/insert_book_log.mjs
@@ -11,7 +11,7 @@
 // --date is optional. default date is current day
 //
 
-const BOOK_LOG_MDX_PATH = "content/posts/2022/10/20221014/index.mdx"
+const BOOK_LOG_MDX_PATH = "content/posts/2023/01/20230105/index.mdx"
 
 const bookName = argv._[1]
 if (bookName === undefined) {
@@ -23,9 +23,9 @@ const date = argv.date || (new Date()).toISOString().substr(0, 10)
 const bookLog = `\n|${bookName}|${date}||`
 fs.outputFileSync(BOOK_LOG_MDX_PATH, bookLog, {flag: "a+"})
 
-const commitLog = "update book log"
+const commitLog = "feat: update book log"
 
 await $`git add ${BOOK_LOG_MDX_PATH}`
-await $`git commit -m "${commitLog}"`
+await $`git commit -m ${commitLog}`
 
 console.log("updated book log and committed.")
