e24da944fc0bdcf614c71f6d2a5279f662053d52
youknowcast <ykc@daycrift.net>
2026-08-15 12:31:20 +0900

    feat: remove share links from post pages

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -34,12 +34,4 @@ export default defineAstroPaperConfig({
     { name: "zenn",   url: "https://zenn.dev/youknowcast" },
     { name: "qiita",  url: "https://qiita.com/youknowcast" },
   ],
-  shareLinks: [
-    { name: "whatsapp", url: "https://wa.me/?text=" },
-    { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
-    { name: "x",        url: "https://x.com/intent/post?url=" },
-    { name: "telegram", url: "https://t.me/share/url?url=" },
-    { name: "pinterest", url: "https://pinterest.com/pin/create/button/?url=" },
-    { name: "mail",     url: "mailto:?subject=See%20this%20post&body=" },
-  ],
 });
